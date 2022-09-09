0dac2cb6e5d89b8b44a7d3e30f3ee6fef763deda
Angela <angelanacpil83@gmail.com>
2022-09-09 15:58:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./tailwind.output.css";
 
 function App() {
   const [mobileToggle, setMobileToggle] = useState(false);
+  const [search, setSearch] = useState(false);
 
   const handleMenu = (evt) => {
     setMobileToggle(!mobileToggle);
+  };
+
+  const handleSearch = (evt) => {
+    setSearch(!search);
   };
 
   return (
@@ -30,7 +35,8 @@ function App() {
 
           <div
             className={`md:flex text-left w-full
-          ${mobileToggle ? "flex flex-col gap-3" : "hidden"}`}
+          ${mobileToggle ? "flex flex-col gap-3" : "hidden"}
+          ${search ? "" : ""}`}
           >
             {/* HIDDEN */}
             <ul className="md:flex gap-10">
@@ -52,14 +58,23 @@ function App() {
                 <button>Cart</button>
               </li>
               <li className="py-2">
-                <button>Search</button>
+                <button onClick={handleSearch}>Search</button>
               </li>
             </ul>
           </div>
+
+          {/* SEARCH INPUT DROPDOWN - follow menu? */}
+          <form
+            className={`w-full flex justify-between gap-5 md:py-3 ${
+              search ? "" : "hidden"
+            }`}
+          >
+            <input className="border-4 w-full" />
+          </form>
         </nav>
 
         {/* MAIN */}
-        <main className="bg-orange-300 p-10 w-100 h-100">
+        <main className="bg-orange-300 p-10">
           {/* Product List - Container of cards */}
           <div className="grid gap-10 h-100 grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
             <ProductCard></ProductCard>
@@ -77,7 +92,35 @@ function App() {
           </div>
         </main>
 
-        <footer className="bg-slate-500 p-4">Footer</footer>
+        {/* FOOTER */}
+        <footer className="bg-slate-500 py-4 px-10 flex flex-col md:flex-row gap-5 md:text-left flex-wrap items-center md:items-stretch">
+          <div className="flex-1 w-64">
+            <p>The Capsule</p>
+            <p>subtitle.</p>
+          </div>
+
+          <div className="flex-1 w-50">
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+          </div>
+
+          <div className="flex-1 w-50">
+            <p>Link</p>
+            <p>Link</p>
+            <p>Link</p>
+          </div>
+
+          <hr className="w-full" />
+
+          <div className="flex justify-evenly w-full">
+            <p>Icon</p>
+            <p>Icon</p>
+            <p>Icon</p>
+          </div>
+
+          <p className="text-center w-full">@2022 Angela Nacpil</p>
+        </footer>
       </div>
     </>
   );
