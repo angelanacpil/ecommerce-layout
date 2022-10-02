34a1db874906299ed63380e3d6336b38495810d5
Angela Nacpil <angelanacpil83@gmail.com>
2022-10-02 13:01:20 +0800

    Do: Move to Official

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        heading: ['Darker Grotesque', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '128': '32rem',
+          '144': '36rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    }
+  }
